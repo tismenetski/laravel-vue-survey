ae008a00e4da1124ac942e4958a100b12bda9dd7
tismenetski <tismenetski@gmail.com>
2022-02-01 15:16:35 +0200

    Login route frontend and backend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 
@@ -34,5 +35,36 @@ class AuthController extends Controller
         ]);
 
 
+    }
+
+    public function login(Request $request){
+
+        $credentials = $request->validate([
+
+            'email' => 'required|email|string|exists:users,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+
+        if (!Auth::attempt($credentials,$remember)) {
+            return response([
+                'error' => 'The Provided Credentials are incorrect'
+            ], 422);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
     }
 }
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -11,22 +11,22 @@
             </router-link>
         </p>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <form class="mt-8 space-y-6" @submit="login">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
             <div>
                 <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                <input id="email-address" name="email" v-model="user.email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
             </div>
             <div>
                 <label for="password" class="sr-only">Password</label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                <input id="password" name="password" v-model="user.password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
             </div>
         </div>
 
         <div class="flex items-center justify-between">
             <div class="flex items-center">
-                <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
+                <input id="remember-me" name="remember-me" v-model="user.remember" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
                 <label for="remember-me" class="ml-2 block text-sm text-gray-900">
                     Remember me
                 </label>
@@ -53,9 +53,25 @@
     </form>
 </template>
 
-<script>
-export default {
-    name: "Login"
+<script setup>
+import store from '../store'
+import {useRouter} from 'vue-router'
+
+const router = useRouter();
+
+const user = {
+    email : '',
+    password : '',
+    remember : false
+
+}
+
+function login(ev) {
+    ev.preventDefault();
+    store.dispatch('login', user).then((res)=> {
+        router.push({name : 'Dashboard'});
+    })
+
 }
 </script>
 
