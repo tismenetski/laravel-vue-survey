365816f01ac0a43efd7d298a05d6bd7fe8801eec
tismenetski <tismenetski@gmail.com>
2022-02-01 14:30:48 +0200

    Register Route Both Back and front

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -11,20 +11,24 @@
             </router-link>
         </p>
     </div>
-    <form class="mt-8 space-y-6" action="#" method="POST">
+    <form class="mt-8 space-y-6" @submit="register">
         <input type="hidden" name="remember" value="true">
         <div class="rounded-md shadow-sm -space-y-px">
             <div>
                 <label for="fullname" class="sr-only">Full Name</label>
-                <input id="fullname" name="name" type="text" autocomplete="name" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Full Name">
+                <input id="fullname" name="name" v-model="user.name" type="text" autocomplete="name" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Full Name">
             </div>
             <div>
                 <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                <input id="email-address" name="email" v-model="user.email"  type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
             </div>
             <div>
                 <label for="password" class="sr-only">Password</label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                <input id="password" name="password" v-model="user.password"  type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900  focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+            </div>
+            <div>
+                <label for="password_confirmation" class="sr-only">Password Confirmation</label>
+                <input id="password_confirmation" name="password_confirmation" v-model="user.password_confirmation"  type="password" autocomplete="current-password_confirmation" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password Confirmation">
             </div>
         </div>
         <div>
@@ -41,12 +45,22 @@
     </form>
 </template>
 
-<script>
-export default {
-    name: "Register"
+<script setup>
+import store from '../store'
+import {useRouter} from 'vue-router'
+
+const router = useRouter();
+const user = {
+    name : '',
+    email : '',
+    password : '',
+    password_confirmation : ''
+}
+
+function register(ev) {
+    ev.preventDefault();
+    store.dispatch('register',user).then((res) => {
+        router.push({name : 'Dashboard'});
+    })
 }
 </script>
-
-<style scoped>
-
-</style>
