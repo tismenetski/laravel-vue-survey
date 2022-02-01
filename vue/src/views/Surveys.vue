2916b39c0505509e8b6c733718405ccdd77aa4be
tismenetski <tismenetski@gmail.com>
2022-02-01 10:52:47 +0200

    Created Default layout

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,9 +1,9 @@
-
+<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold text-gray-900">
-                Dashboard
+                Surveys
             </h1>
         </div>
     </header>
@@ -19,7 +19,9 @@
 </template>
 
 <script>
+
 export default {
-    name : "Dashboard"
+
+
 }
 </script>
