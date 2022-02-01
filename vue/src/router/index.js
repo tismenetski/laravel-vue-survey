c0405392f4fcafc25b829ec58eec9ea75e21d2f3
tismenetski <tismenetski@gmail.com>
2022-02-01 10:53:02 +0200

    Securing Dashboard path

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -2,12 +2,20 @@ import {createRouter, createWebHistory} from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Surveys from "../views/Surveys.vue";
 
 const routes = [
     {
         path : '/',
-        name : 'Dashboard',
-        component : Dashboard
+        redirect : '/dashboard',
+        component : DefaultLayout,
+        meta : {requiresAuth : true},
+        children : [
+            {path: '/dashboard', name : 'Dashboard', component : Dashboard},
+            {path: '/surveys', name : 'Surveys', component : Surveys},
+
+        ]
     },
     {
         path : '/login',
@@ -19,6 +27,7 @@ const routes = [
         name : 'Register',
         component : Register
     }
+
 ];
 
 const router = createRouter( {
@@ -26,6 +35,17 @@ const router = createRouter( {
     history : createWebHistory(),
     routes
 
+})
+
+// before the user reaches a secured page we check if the user have a token, if token exists we allow him
+// to continue to the path, otherwise we send him to login page
+router.beforeEach((to,from,next) => {
+
+    if (to.meta.requiresAuth && !store.state.user.token){
+        next({name: 'Login'})
+    } else {
+        next();
+    }
 })
 
 
