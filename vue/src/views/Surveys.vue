30e267b4409d4065ea27515ed241093378e76179
tismenetski <tismenetski@gmail.com>
2022-02-01 13:30:57 +0200

    Added models and migrations

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,5 +1,6 @@
 <template>
     <page-component title="Surveys">Content Goes Here</page-component>
+<!--    This uses the page component, page component have prop named title , so we pass the title, everything between the page-component start and end will be the content of slot   -->
 </template>
 
 <script setup>
