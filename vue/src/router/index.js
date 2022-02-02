fea3cfd13434b61a624ac7432a732bf66c7b0473
tismenetski <tismenetski@gmail.com>
2022-02-02 12:24:08 +0200

    Added survey edit/create routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,6 +6,7 @@ import DefaultLayout from "../components/DefaultLayout.vue";
 import Surveys from "../views/Surveys.vue";
 import store from "../store";
 import AuthLayout from "../components/AuthLayout.vue";
+import SurveyView from "../views/SurveyView.vue";
 
 const routes = [
     {
@@ -16,6 +17,8 @@ const routes = [
         children : [
             {path: '/dashboard', name : 'Dashboard', component : Dashboard},
             {path: '/surveys', name : 'Surveys', component : Surveys},
+            {path: '/surveys/create', name : 'SurveyCreate', component : SurveyView},
+            {path: '/surveys/:id', name : 'SurveyView', component : SurveyView},
 
         ]
     },
